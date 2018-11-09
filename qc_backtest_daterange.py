d7a4aface66922f86bac4483227085d75c63ea80
d-fay <fay.dustin@gmail.com>
2018-11-08 23:56:32 -0700

    add initial poc qc backtest script

---
+++ qc_backtest_daterange.py
@@ -0,0 +1,143 @@
+# -*- coding: utf-8 -*-
+import argparse
+import logging.handlers
+import os
+import time
+import urllib.request
+
+from dotenv import load_dotenv
+
+from code.qc.algolab import models
+from code.qc.api.wrapper import QCApi
+
+# dir_path = os.path.dirname(os.path.realpath(__file__))
+# dotenv_path = os.path.join(os.getcwd(), '.env')
+# load_dotenv(dotenv_path)
+
+# uid = os.environ['QC_USER_ID'],
+# token = os.environ['QC_ACCESS_TOKEN']
+# pid = os.environ['QC_BACKTEST_DATERANGE_PID']
+
+
+class QCHelper(QCApi):
+    """ Toolkit for the QuantConnect CommandLine AlgorithmLab API Interactor Tool """
+
+    def __init__(self):
+        self.dir_qc_proj = os.path.dirname(os.path.realpath(models.__file__))
+        self.dir_script = os.path.dirname(os.path.realpath(__file__))
+        self.dotenv_path = os.path.join(os.getcwd(), '.env')
+        load_dotenv(self.dotenv_path)
+        self.pid = os.environ['QC_BACKTEST_DATERANGE_PID']
+        super(QCHelper, self).__init__(userId=os.environ['QC_USER_ID'], token=os.environ['QC_ACCESS_TOKEN'])
+
+    def get_project_directory(self):
+
+        project_response = self.read_project(self.pid)
+        project_details = project_response['projects'][0]
+        filename = os.path.join(self.dir_qc_proj, 'project_files/{}_{}'.format(
+            project_details['projectId'],
+            project_details['name']))
+        return filename
+
+    def update_config_params(self, config_file_txt):
+        self.update_project_file_content(projectId=self.pid,
+                                         fileName='conf.py',
+                                         newFileContents=config_file_txt)
+
+
+def setup_logging():
+    qc = QCHelper()
+    log_dir = os.path.join(qc.dir_script, 'log')
+    if not os.path.isdir(log_dir):
+        os.mkdir(os.path.join(qc.dir_script, 'log'))
+    log_filename = 'log/algolab.log'
+    log = logging.getLogger('algolab_logger')
+    log.setLevel(logging.DEBUG)
+
+    # Add handler to create new log file every 10MB
+    file_handler = logging.handlers.RotatingFileHandler(
+        log_filename, maxBytes=10*1024*1024, backupCount=5)
+    log.addHandler(file_handler)
+
+
+def update_project_params(start_date,
+                          end_date):
+    config_file_txt = 'START_DATE = \'{}\'\n' \
+                      'END_DATE = \'{}\'\n'.format(start_date[0], end_date[0])
+    qc = QCHelper()
+    cur_proj_dir = qc.get_project_directory()
+    config_filepath = os.path.join(cur_proj_dir, 'conf.py')
+    # save file locally
+    with open(config_filepath, 'w') as f:
+        f.write(config_file_txt)
+    # update remotely
+    qc.update_config_params(config_file_txt)
+
+
+def compile_project():
+    qc = QCHelper()
+    print('Compiling project...')
+    compile_results = qc.create_compile(qc.pid)
+    compile_id = compile_results['compileId']
+    print('Checking compilation results')
+    while True:
+        compile_read_status_results = qc.read_compile(qc.pid, compile_id)
+
+        if compile_read_status_results['state'] == 'InQueue':
+            time.sleep(15)  # seconds
+            print('Compilation still in queue: waiting.')
+        elif compile_read_status_results['state'] == 'BuildError':
+            print(compile_read_status_results['logs'])
+            exit(0)
+        elif compile_read_status_results['state'] == 'BuildSuccess':
+            print(compile_read_status_results['logs'])
+            return compile_id
+        else:
+            print(compile_read_status_results)
+            print('Error')
+            exit(0)
+
+
+def backtest_compiled_project(compile_id):
+    qc = QCHelper()
+    print('Triggering project backtest...')
+    backtest_results = qc.create_backtest(qc.pid, compile_id, 'backtest_name_02')
+    backtest_id = backtest_results['backtestId']
+    backtest_read_results = qc.read_backtest(qc.pid, backtest_id)
+    print(backtest_read_results)
+    while backtest_read_results['progress'] < 1:
+        print('Waiting for backtest to complete...')
+        time.sleep(15)
+        backtest_read_results = qc.read_backtest(qc.pid, backtest_id)
+    if backtest_read_results['completed'] is True:
+        print(backtest_read_results['result'])
+        print('Successful backtest! Downloading log file')
+
+        log_url = 'https://www.quantconnect.com/backtests/{}/{}/{}-log.txt'.format(
+            os.environ['QC_USER_ID'], qc.pid, backtest_id)
+
+        filename = '{}-log.txt'.format(backtest_id)
+        log_dir = os.path.join(qc.get_project_directory(), 'backtest_logs')
+        if not os.path.exists(log_dir):
+            os.makedirs(log_dir)
+        file_path = os.path.join(log_dir, filename)
+        urllib.request.urlretrieve(log_url, file_path)
+        return file_path
+
+
+if __name__ == '__main__':
+    setup_logging()
+
+    """ Parse arguments from CLI """
+    # python algolab.py --push_project 1231453647441324, live-algo-name, <code>
+    parser = argparse.ArgumentParser(description='Backtest date param passing demo')
+    parser.add_argument('-s', '--start_date', nargs=1, metavar=('<start-date>',), default=None, help="First date to run algo on")
+    parser.add_argument('-e', '--end_date', nargs=1, metavar=('<end-date>',), default=None, help="Last date to run algo on")
+
+    args = parser.parse_args()
+
+    update_project_params(args.start_date,
+                          args.end_date)
+    compile_id = compile_project()
+    backtest_compiled_project(compile_id)
+    print('Done.')
