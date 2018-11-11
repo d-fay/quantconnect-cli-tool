7e2c158ad990ebcf554fd7b6db0ccdea04470cf2
d-fay <fay.dustin@gmail.com>
2018-11-11 00:45:18 -0700

    update daterange params to dict and misc cleanup qc_backtest_daterange.py

--- qc_backtest_daterange.py
+++ qc_backtest_daterange.py
@@ -1,23 +1,18 @@
 # -*- coding: utf-8 -*-
 import argparse
+import datetime as dt
 import logging.handlers
 import os
 import time
-import datetime as dt
 import urllib.request
 
 from dotenv import load_dotenv
 
 from code.qc.algolab import models
 from code.qc.api.wrapper import QCApi
+# from code.qc.algolab.models import AlgorithmLabToolkit
 
-# dir_path = os.path.dirname(os.path.realpath(__file__))
-# dotenv_path = os.path.join(os.getcwd(), '.env')
-# load_dotenv(dotenv_path)
-
-# uid = os.environ['QC_USER_ID'],
-# token = os.environ['QC_ACCESS_TOKEN']
-# pid = os.environ['QC_BACKTEST_DATERANGE_PID']
+params_filename = 'params.py'
 
 
 class QCHelper(QCApi):
@@ -29,10 +24,11 @@ class QCHelper(QCApi):
         self.dotenv_path = os.path.join(os.getcwd(), '.env')
         load_dotenv(self.dotenv_path)
         self.pid = os.environ['QC_BACKTEST_DATERANGE_PID']
-        super(QCHelper, self).__init__(userId=os.environ['QC_USER_ID'], token=os.environ['QC_ACCESS_TOKEN'])
+        self.uid = os.environ['QC_USER_ID']
+        super(QCHelper, self).__init__(userId=os.environ['QC_USER_ID'],
+                                       token=os.environ['QC_ACCESS_TOKEN'])
 
     def get_project_directory(self):
-
         project_response = self.read_project(self.pid)
         project_details = project_response['projects'][0]
         filename = os.path.join(self.dir_qc_proj, 'project_files/{}_{}'.format(
@@ -42,7 +38,7 @@ class QCHelper(QCApi):
 
     def update_config_params(self, config_file_txt):
         self.update_project_file_content(projectId=self.pid,
-                                         fileName='conf.py',
+                                         fileName=params_filename,
                                          newFileContents=config_file_txt)
 
 
@@ -54,7 +50,6 @@ def setup_logging():
     log_filename = 'log/algolab.log'
     log = logging.getLogger('algolab_logger')
     log.setLevel(logging.DEBUG)
-
     # Add handler to create new log file every 10MB
     file_handler = logging.handlers.RotatingFileHandler(
         log_filename, maxBytes=10*1024*1024, backupCount=5)
@@ -63,16 +58,16 @@ def setup_logging():
 
 def update_project_params(start_date,
                           end_date):
-    config_file_txt = 'START_DATE = \'{}\'\n' \
-                      'END_DATE = \'{}\'\n'.format(start_date[0], end_date[0])
+    # config_file_txt = 'START_DATE = \'{}\'\n' \
+    #                   'END_DATE = \'{}\'\n'.format(start_date, end_date)
+    daterange = {'start_date': start_date, 'end_date': end_date}
+    params_file_contents = 'daterange = {}\n'.format(daterange)
     qc = QCHelper()
     cur_proj_dir = qc.get_project_directory()
-    config_filepath = os.path.join(cur_proj_dir, 'conf.py')
-    # save file locally
-    with open(config_filepath, 'w') as f:
-        f.write(config_file_txt)
-    # update remotely
-    qc.update_config_params(config_file_txt)
+    params_filepath = os.path.join(cur_proj_dir, params_filename)
+    with open(params_filepath, 'w') as f:
+        f.write(params_file_contents)                # save file locally
+    qc.update_config_params(params_file_contents)    # update remotely
 
 
 def compile_project():
@@ -106,39 +101,46 @@ def backtest_compiled_project(compile_id):
         qc.pid, str(dt.datetime.now())[:19].replace(' ', '_').replace(':', ''))
     backtest_results = qc.create_backtest(qc.pid, compile_id, backtest_name)
     backtest_id = backtest_results['backtestId']
-    backtest_read_results = qc.read_backtest(qc.pid, backtest_id)
-    while backtest_read_results['progress'] < 1:
+    while True:
+        backtest_read_results = qc.read_backtest(qc.pid, backtest_id)
+        if backtest_read_results['completed'] is True and backtest_read_results['progress'] == 1:
+            print(backtest_read_results['result'])
+            print('Successful backtest! Downloading log file...')
+            log_url = 'https://www.quantconnect.com/backtests/{}/{}/{}-log.txt'.format(
+                qc.uid, qc.pid, backtest_id)
+            filename = '{}_{}-log.txt'.format(backtest_name, backtest_id)
+            backtest_log_dir = os.path.join(qc.get_project_directory(), 'backtest_logs')
+            if not os.path.exists(backtest_log_dir):
+                os.makedirs(backtest_log_dir)
+            file_path = os.path.join(backtest_log_dir, filename)
+            urllib.request.urlretrieve(log_url, file_path)
+            print('Backtest log file saved to: {}'.format(file_path))
+            return file_path
         print('Waiting for backtest to complete...')
         time.sleep(15)
-        backtest_read_results = qc.read_backtest(qc.pid, backtest_id)
-    if backtest_read_results['completed'] is True:
-        print(backtest_read_results['result'])
-        print('Successful backtest! Downloading log file...')
 
-        log_url = 'https://www.quantconnect.com/backtests/{}/{}/{}-log.txt'.format(
-            os.environ['QC_USER_ID'], qc.pid, backtest_id)
 
-        filename = '{}_{}-log.txt'.format(backtest_name, backtest_id)
-        backtest_log_dir = os.path.join(qc.get_project_directory(), 'backtest_logs')
-        if not os.path.exists(backtest_log_dir):
-            os.makedirs(backtest_log_dir)
-        file_path = os.path.join(backtest_log_dir, filename)
-        urllib.request.urlretrieve(log_url, file_path)
-        return file_path
+def read_file_to_log(file_path):
+    with open(file_path, 'r') as f:
+        lines = f.read().splitlines()
+        log_txt = '\n'
+        for line in lines:
+            log_txt += '{}\n'.format(line)
+        print(log_txt)
 
 
 if __name__ == '__main__':
     setup_logging()
-
     """ Parse arguments from CLI """
     parser = argparse.ArgumentParser(description='Backtest date param passing demo')
-    parser.add_argument('-s', '--start_date', nargs=1, metavar=('<start-date>',), default=None, help="First date to run algo on")
-    parser.add_argument('-e', '--end_date', nargs=1, metavar=('<end-date>',), default=None, help="Last date to run algo on")
-
+    parser.add_argument('-s', '--start_date', metavar=('<start-date>',),
+                        default=None, help="First date to run algo on")
+    parser.add_argument('-e', '--end_date', metavar=('<end-date>',),
+                        default=None, help="Last date to run algo on")
     args = parser.parse_args()
-
     update_project_params(args.start_date,
                           args.end_date)
-    compile_id = compile_project()
-    backtest_compiled_project(compile_id)
+    compilation_id = compile_project()
+    backtest_log_filepath = backtest_compiled_project(compilation_id)
+    read_file_to_log(backtest_log_filepath)
     print('Done.')
